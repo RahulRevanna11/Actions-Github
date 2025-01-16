edac9d6feb11baa65b5aa1aaf0545c5cf0b7cc02
Rahul <rahulrevanna11@gmail.com>
2025-01-16 19:31:15 +0530

    deleted index.js

---
+++ .github/scripts/setenv.sh
@@ -0,0 +1,14 @@
+# .github/scripts/setenv.sh
+
+#!/bin/bash
+set -eux
+
+while [ $# -gt 0 ]; do
+    case $1 in
+        --ref) GITHUB_REF=$2; shift 2; ;;
+        *) shift ;;
+    esac
+done
+
+echo BRANCH_SCRIPT=${GITHUB_REF} | tee -a "$GITHUB_ENV"
+echo branch=${GITHUB_REF} | tee -a "$GITHUB_OUTPUT"
